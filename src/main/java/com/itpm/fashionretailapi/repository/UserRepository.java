15fefbb9f5e2fae898eb1853b58463d36db88402
xcvvxv <xcvvxv>
2024-03-28 18:56:48 +0530

    item added to the cart

--- src/main/java/com/itpm/fashionretailapi/repository/UserRepository.java
+++ src/main/java/com/itpm/fashionretailapi/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.itpm.fashionretailapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByName(String name);
 }
