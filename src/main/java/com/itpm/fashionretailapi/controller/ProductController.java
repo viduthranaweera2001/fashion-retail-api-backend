c070c6e2abe6ed9d1dc266307fffce57c90bc873
viduthranaweera2001 <viduthranaweera1234@gmail.com>
2024-03-28 20:00:37 +0530

    product add

--- src/main/java/com/itpm/fashionretailapi/controller/ProductController.java
+++ src/main/java/com/itpm/fashionretailapi/controller/ProductController.java
@@ -23,5 +23,4 @@ public class ProductController {
         ProductResponse productResponse = productService.createProduct(id,productRequestDto);
          return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
     }
-
 }
