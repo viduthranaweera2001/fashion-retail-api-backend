a77789c28fbc70a2460de5ee3d35e89db8d2055b
viduthranaweera2001 <viduthranaweera1234@gmail.com>
2024-03-29 12:42:07 +0530

    order status updated

--- src/main/java/com/itpm/fashionretailapi/controller/request/OrderRequest.java
+++ src/main/java/com/itpm/fashionretailapi/controller/request/OrderRequest.java
@@ -1,11 +1,14 @@
 package com.itpm.fashionretailapi.controller.request;
 
+import com.itpm.fashionretailapi.model.Status;
 import lombok.Data;
 
 @Data
 public class OrderRequest {
+    private String userName;
     private String address;
     private String city;
     private String phoneNumber;
     private String postalCode;
+    private String status;
 }
--- src/main/java/com/itpm/fashionretailapi/service/impl/OrderServiceImpl.java
+++ src/main/java/com/itpm/fashionretailapi/service/impl/OrderServiceImpl.java
@@ -2,7 +2,10 @@ package com.itpm.fashionretailapi.service.impl;
 
 import com.itpm.fashionretailapi.controller.dto.OrderRequestDto;
 import com.itpm.fashionretailapi.controller.response.IdResponse;
+import com.itpm.fashionretailapi.controller.response.OrderResponse;
 import com.itpm.fashionretailapi.exception.CartNotFoundException;
+import com.itpm.fashionretailapi.exception.CustomerNotFoundException;
+import com.itpm.fashionretailapi.exception.OrderNotFoundException;
 import com.itpm.fashionretailapi.model.Cart;
 import com.itpm.fashionretailapi.model.Order;
 import com.itpm.fashionretailapi.model.Status;
@@ -15,6 +18,9 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class OrderServiceImpl implements OrderService {
@@ -22,6 +28,7 @@ public class OrderServiceImpl implements OrderService {
     private ModelMapper modelMapper;
     private CartRepository cartRepository;
     private OrderRepository orderRepository;
+    private UserRepository userRepository;
     @Override
     public IdResponse addOrder(Long id, OrderRequestDto orderRequestDto) throws CartNotFoundException {
         Cart cart = cartRepository.findById(id).orElseThrow(
@@ -33,11 +40,48 @@ public class OrderServiceImpl implements OrderService {
         order.setStatus(Status.PROCESSING);
         order.setShippingPrice(Float.valueOf("194.00"));
         order.setTotalPrice((float) (194.00+ cart.getPrice()));
+        order.setUser(cart.getUser());
+
+        order.setName(cart.getUser().getName());
 
         orderRepository.save(order);
 
         return IdResponse.builder()
                 .message(cart.getUser().getName()+" ordered "+cart.getProduct().getName())
+                .build();
+    }
+
+    @Override
+    public List<OrderResponse> getOrdersByUser(Long Uid)throws CustomerNotFoundException,OrderNotFoundException{
+        User user = userRepository.findById(Uid).orElseThrow(
+                ()->new CustomerNotFoundException("user Not found with id "+Uid)
+        );
+        List<Order> orderList = orderRepository.findOrdersByUser(user);
+
+        if (orderList.isEmpty())
+            throw new OrderNotFoundException("Order Not Found");
+
+        return orderList.stream()
+                .map(order -> modelMapper.map(order,OrderResponse.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public IdResponse updateStatus(Long id, OrderRequestDto orderRequestDto) throws OrderNotFoundException {
+        Order order = orderRepository.findById(id).orElseThrow(
+                ()->new OrderNotFoundException("Order Not found with id "+id)
+        );
+
+        if (orderRequestDto.getStatus().equalsIgnoreCase("DELIVERED"))
+            order.setStatus(Status.DELIVERED);
+
+        if (orderRequestDto.getStatus().equalsIgnoreCase("PROCESSING"))
+            order.setStatus(Status.PROCESSING);
+
+        orderRepository.save(order);
+
+        return IdResponse.builder()
+                .message("Order status updated to "+orderRequestDto.getStatus())
                 .build();
     }
 }
